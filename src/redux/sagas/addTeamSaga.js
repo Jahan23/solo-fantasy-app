b88f81ba0b9d5e9ec81fc1ef674c0cfdfcaeb3ab
Jahan Powell <jahanpowell23@gmail.com>
2020-07-07 18:55:09 -0500

    create, read and update functioning

--- src/redux/sagas/addTeamSaga.js
+++ src/redux/sagas/addTeamSaga.js
@@ -5,19 +5,19 @@ function* addTeam(action){
     try {
       const config = {
         name: action.payload.name, id: action.payload.id
-
       };
   
       // the config includes credentials which
       // allow the server session to recognize the user
       // If a user is logged in, this will return their information
       // from the server session (req.user)
-      const response = yield axios.post(`/api/user/teams`, config);
+      yield axios.post(`/api/user/teams`, config);
+      const getTeamsResponse = yield axios.get(`/api/user/teams/${action.payload.id}`);
   
       // now that the session has given us a user object
       // with an id and username set the client-side user object to let
       // the client-side code know the user is logged in
-      yield put({ type: 'ADD_TEAM_NAME', payload: response.data });
+      yield put({ type: 'SET_TEAMS', payload: getTeamsResponse.data });
     } catch (error) {
       console.log('User get request failed', error);
     }
